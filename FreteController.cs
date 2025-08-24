fe5a3b576006cfd71d7268034c067586cc697e34
rai0812 <raissa14m@gmail.com>
2025-08-24 19:05:47 -0300

    Add files via upload

---
+++ FreteController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeuProjeto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FreteController : ControllerBase
+    {
+        
+        public class Produto
+        {
+            public string Nome { get; set; }
+            public float Peso { get; set; }         
+            public float Altura { get; set; }       
+            public float Largura { get; set; }     
+            public float Comprimento { get; set; }  
+            public string UF { get; set; }          
+        }
+
+        
+        private readonly Dictionary<string, decimal> tarifasEstado = new()
+        {
+            { "SP", 50.00m },
+            { "RJ", 60.00m },
+            { "MG", 55.00m }
+        };
+
+        private const decimal taxaPorCm3 = 0.01m; 
+        
+        [HttpPost("calcular")]
+        public IActionResult CalcularFrete([FromBody] Produto produto)
+        {
+            if (produto.Altura <= 0 || produto.Largura <= 0 || produto.Comprimento <= 0)
+                return BadRequest("Altura, largura e comprimento devem ser maiores que zero.");
+
+            
+            decimal volume = (decimal)(produto.Altura * produto.Largura * produto.Comprimento);
+
+           
+            decimal tarifaEstado = tarifasEstado.ContainsKey(produto.UF.ToUpper())
+                ? tarifasEstado[produto.UF.ToUpper()]
+                : 70.00m;
+
+            
+            decimal valorFrete = (volume * taxaPorCm3) + tarifaEstado;
+
+            return Ok(new
+            {
+                produto.Nome,
+                produto.Peso,
+                produto.Altura,
+                produto.Largura,
+                produto.Comprimento,
+                produto.UF,
+                Volume = volume,
+                ValorFrete = valorFrete.ToString("C2") 
+            });
+        }
+    }
+}
---
+++ PessoaController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeuProjeto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public double Peso { get; set; }
+            public double Altura { get; set; }
+        }
+
+    
+        [HttpPost("calcular-imc")]
+        public IActionResult CalcularIMC([FromBody] Pessoa pessoa)
+        {
+            if (pessoa.Altura <= 0)
+                return BadRequest("Altura deve ser maior que zero.");
+
+            double imc = pessoa.Peso / (pessoa.Altura * pessoa.Altura);
+
+            return Ok(new
+            {
+                pessoa.Nome,
+                IMC = imc.ToString("F2")
+            });
+        }
+
+      
+        [HttpGet("consulta-tabela-imc")]
+        public IActionResult ConsultaTabelaIMC(double imc)
+        {
+            string descricao;
+
+            if (imc < 18.5)
+                descricao = "Abaixo do peso";
+            else if (imc < 24.9)
+                descricao = "Peso normal";
+            else if (imc < 29.9)
+                descricao = "Sobrepeso";
+            else if (imc < 34.9)
+                descricao = "Obesidade Grau I";
+            else if (imc < 39.9)
+                descricao = "Obesidade Grau II";
+            else
+                descricao = "Obesidade Grau III (mórbida)";
+
+            return Ok(new
+            {
+                IMC = imc.ToString("F2"),
+                Classificacao = descricao
+            });
+        }
+    }
+}
+
